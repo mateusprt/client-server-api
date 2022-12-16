03a66cb2398172e62624d73077754fe86740e33f
mateusprt <matnun95@gmail.com>
2022-12-16 17:23:41 -0300

    Correções

--- client/client.go
+++ client/client.go
@@ -17,19 +17,27 @@ func main() {
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 
 	if err != nil {
-		panic(err)
+		fmt.Println("Error on request")
+		return
 	}
 
 	res, err := http.DefaultClient.Do(req)
 
 	if err != nil {
-		panic(err)
+		fmt.Println("Something bad happened")
+		return
 	}
 
 	defer res.Body.Close()
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Erro ao ler a resposta: %v\n", err)
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(res.StatusCode, "The server is taking to long to respond")
+		return
 	}
 
 	file, err := os.Create("cotacoes.txt")
--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -26,6 +27,10 @@ type Cotacao struct {
 		Timestamp  string `json:"timestamp"`
 		CreateDate string `json:"create_date"`
 	} `json:"USDBRL"`
+}
+
+type RestClient struct {
+	Url string
 }
 
 func main() {
@@ -81,6 +86,47 @@ func cotacaoHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(statusCode)
 	w.Write([]byte(cotacao.Usdbrl.Bid))
+}
+
+func NewRestClient(url string) *RestClient {
+	return &RestClient{Url: url}
+}
+
+func (r RestClient) Get() ([]byte, int) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", r.Url, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		return nil, http.StatusInternalServerError
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	select {
+	case <-ctx.Done():
+		log.Println("[STATUS 408 (REQUEST TIMEOUT)]: The server is taking to long to respond.")
+		return nil, http.StatusRequestTimeout
+	case <-time.After(100 * time.Millisecond):
+		if err != nil {
+			log.Println(err.Error())
+			return nil, http.StatusInternalServerError
+		}
+
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+
+		if err != nil {
+			log.Println(err.Error())
+			return nil, http.StatusInternalServerError
+		}
+		log.Println("[STATUS 200 (OK)]: Request successfully completed.")
+		return body, http.StatusOK
+	}
 }
 
 func insertCotacao(db *sql.DB, cotacao *Cotacao) error {
